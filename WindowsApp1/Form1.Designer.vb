ad7d2727e1fd7399f86d01dd8d52bbe4b6760793
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-03-09 13:29:53 +0000

    Dodaj pliki projektów.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ReturnToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ForwardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchBox = New System.Windows.Forms.ToolStripTextBox()
+        Me.Search = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ref = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ReturnToolStripMenuItem, Me.ForwardToolStripMenuItem, Me.SearchBox})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1249, 27)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ReturnToolStripMenuItem
+        '
+        Me.ReturnToolStripMenuItem.Name = "ReturnToolStripMenuItem"
+        Me.ReturnToolStripMenuItem.Size = New System.Drawing.Size(51, 23)
+        Me.ReturnToolStripMenuItem.Text = "return"
+        '
+        'ForwardToolStripMenuItem
+        '
+        Me.ForwardToolStripMenuItem.Name = "ForwardToolStripMenuItem"
+        Me.ForwardToolStripMenuItem.Size = New System.Drawing.Size(60, 23)
+        Me.ForwardToolStripMenuItem.Text = "forward"
+        '
+        'SearchBox
+        '
+        Me.SearchBox.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.SearchBox.Name = "SearchBox"
+        Me.SearchBox.Size = New System.Drawing.Size(500, 23)
+        '
+        'Search
+        '
+        Me.Search.Location = New System.Drawing.Point(626, 1)
+        Me.Search.Name = "Search"
+        Me.Search.Size = New System.Drawing.Size(75, 23)
+        Me.Search.TabIndex = 1
+        Me.Search.Text = "Search"
+        Me.Search.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(118, -2)
+        Me.ProgressBar1.Minimum = 1
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(502, 10)
+        Me.ProgressBar1.TabIndex = 2
+        Me.ProgressBar1.Value = 1
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 30)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(1249, 592)
+        Me.WebBrowser1.TabIndex = 3
+        '
+        'ref
+        '
+        Me.ref.Location = New System.Drawing.Point(707, 1)
+        Me.ref.Name = "ref"
+        Me.ref.Size = New System.Drawing.Size(75, 23)
+        Me.ref.TabIndex = 4
+        Me.ref.Text = "Refresh"
+        Me.ref.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1249, 315)
+        Me.Controls.Add(Me.ref)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Search)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "aa"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ReturnToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ForwardToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SearchBox As ToolStripTextBox
+    Friend WithEvents Search As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents ref As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Private Sub Search_Click(sender As Object, e As EventArgs) Handles Search.Click
+        WebBrowser1.Navigate(SearchBox.Text)
+        Me.Text = WebBrowser1.StatusText
+        Timer1.Enabled = True
+        Me.Text = WebBrowser1.StatusText
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+    End Sub
+    Private Sub Search_Enter(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles Search.KeyPress
+        If e.KeyChar = Convert.ToChar(13) Then
+            WebBrowser1.Navigate(SearchBox.Text)
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = True
+            Me.Text = WebBrowser1.StatusText
+        End If
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+
+    End Sub
+
+
+    Private Sub ReturnToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReturnToolStripMenuItem.Click
+        WebBrowser1.GoBack()
+        Me.Text = WebBrowser1.StatusText
+        Timer1.Enabled = True
+        Me.Text = WebBrowser1.StatusText
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+    End Sub
+
+    Private Sub ForwardToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ForwardToolStripMenuItem.Click
+        WebBrowser1.GoForward()
+        Me.Text = WebBrowser1.StatusText
+        Timer1.Enabled = True
+        Me.Text = WebBrowser1.StatusText
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+    End Sub
+
+    Private Sub Ref_Click(sender As Object, e As EventArgs) Handles ref.Click
+        WebBrowser1.Refresh()
+        Me.Text = WebBrowser1.StatusText
+        Timer1.Enabled = True
+        Me.Text = WebBrowser1.StatusText
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value < ProgressBar1.Maximum - 1 Then
+            ProgressBar1.Value += 1
+        End If
+    End Sub
+
+    Private Sub SearchBox_(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles SearchBox.KeyPress
+        If e.KeyChar = Convert.ToChar(13) Then
+            WebBrowser1.Navigate(SearchBox.Text)
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = True
+            Me.Text = WebBrowser1.StatusText
+        End If
+        If WebBrowser1.IsBusy = True Then
+            Me.Text = WebBrowser1.StatusText
+        Else
+            Me.Text = WebBrowser1.StatusText
+            Timer1.Enabled = False
+            ProgressBar1.Value = ProgressBar1.Maximum
+            ProgressBar1.Value = 1
+        End If
+    End Sub
+
+
+End Class
